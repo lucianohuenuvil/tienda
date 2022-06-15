d4ef6a219857c77f961bfca93d535d41ce31600b
lucianohuenuvil <lucianobvx@gmail.com>
2022-06-15 18:17:48 -0400

    Cambios en el diseño

--- js/main.js
+++ js/main.js
@@ -24,7 +24,45 @@ addEventListener('DOMContentLoaded', () => {
             break;
     }
   }
+
+
+
+  const navresponsive = () => {
+    let buttonNav = document.getElementById("button-responsive");
+    switch (buttonNav.style.left){
+        case "":
+            buttonNav.style.left = "0%"
+            break;
+        case "0%":
+            buttonNav.style.left = "-100%";
+            break;
+        case "-100%":
+            buttonNav.style.left = "0%";
+            break;
+    }
+  } 
   
+
+  
+//Slide scroll page 
+
+const scrollnav = () => $(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 600, function(){
+            
+            window.location.hash = hash;
+        });
+      } 
+    });
+
+});
   
   
   
