f81179990b00033aa43409ca1b5ee6113339e9e7
lucianohuenuvil <lucianobvx@gmail.com>
2022-09-20 21:30:08 -0400

    Responsive fix detail

---
+++ mail.php
@@ -0,0 +1,16 @@
+<? php
+{ if ( isset ( $_POST [ 'enviar' ] ) )
+     if ( ! empty ( $_POST [ 'name' ] ) && !empty ( $_POST [ 'asunto' ] ) && !empty ( $_POST [ 'msg' ] ) && !empty ( $_POST [ 'email' ] ) ) {
+     $name = $_POST [ 'name' ] ;
+     $asunto = $_POST [ 'asunto' ] ;
+     $msg = $_POST [ 'msg' ] ;
+     $email = $_POST [ 'email' ] ;
+     $header = " From : noreply@example.com " " \ r \ n " ;
+     $header . = " Reply - To : noreply@example.com " " \ r \ n " ;
+     $header . = " X - Mailer : PHP / " . phpversion ( ) ;
+     $mail = @mail ( $email , $asunto , $msg , $header ) ;
+     if ( $mail ) {
+     echo " < h4 > ¡ Mail enviado exitosamente ! < / h4 > " ;
+     }
+                                                  
+}
