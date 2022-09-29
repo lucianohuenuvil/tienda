bb8de814dd13be03b993a2bdce24a9b5bb50f3b4
lucianohuenuvil <lucianobvx@gmail.com>
2022-09-28 20:18:44 -0400

    php mail error fixed

--- testmail.php
+++ testmail.php
@@ -2,10 +2,10 @@
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
     $from = "test@hostinger-tutorials.com";
-    $to = $_POST['email'];
+    $to = "lucianobvx@gmail.com"
     $subject = "Asunto: " . $_POST['asunto'];
-    $message = "Nombre: " . $_POST['name'] . "\nNombre del establecimiento: " .  $_POST['establecimiento'] . "\nTeléfono: ". $_POST['number'] . "\nMensaje: ". $_POST['message']; 
+    $message = "Nombre: " . $_POST['name'] . "\nNombre del establecimiento: " .  $_POST['establecimiento'] . "\nTeléfono: ". $_POST['number'] . "\nCorreo: ". $_POST['email'] . "\nMensaje: ". $_POST['message']; 
     $headers = "From: " . "styloschool@formularios.com";
     mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+    echo "The email message was sent."; 
 ?>
