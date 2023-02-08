cb9b7773c25128f570da921376cd596ab38f3815
lucianohuenuvil <lucianobvx@gmail.com>
2023-02-07 21:59:26 -0400

    change send email from contact form

--- testmail.php
+++ testmail.php
@@ -2,7 +2,7 @@
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
     $from = "test@hostinger-tutorials.com";
-    $to = "lucianobvx@gmail.com";
+    $to = "styloschool@gmail.com";
     $subject = "Asunto: " . $_POST['asunto'];
     $message = "Nombre: " . $_POST['name'] . "\nNombre del establecimiento: " .  $_POST['establecimiento'] . "\nTeléfono: ". $_POST['number'] . "\nCorreo: ". $_POST['email'] . "\nMensaje: ". $_POST['message']; 
     $headers = "From: " . "styloschool@formularios.com";
